0e5ee33a197336f6328417e8342da3cf485e56cb
Matt <matthew.simon.barnes@gmail.com>
2017-04-03 20:17:53 -0400

    Fixed basic sim. - Now correctly simulates the environment - Tracks the total customers arrived (ARR) and the   total customers that had to be turned away (BYE)

--- 4470-mod-sim/sim-mech-shop/customer.py
+++ 4470-mod-sim/sim-mech-shop/customer.py
@@ -1,66 +1,17 @@
-import simpy
+def customer(env, name, mech, DAY_LENGTH, job_duration, status):
+    arrive = env.now
+    # Request one of the servers
+    print('ARR\t%s arriving at %d\t|| job_duration: %d' % (name, arrive, job_duration))
 
+    if (status == 1):
+        print('BYE\tSending %s home' % (name)) 
 
-def customer(env, mech, cust_num, interarrival_time, job_duration):
-  # Simulate varied interarrival time
-  yield env.timeout(interarrival_time)
+    else:
+        with mech.request() as req:
+            yield req
 
-  # Request one of the servers
-  print('Customer %d arriving at %d' % (cust_num, env.now))
-  with mech.request() as req:
-    yield req
+            wait = env.now - arrive
+            print('SER\t%s being served at %d' % (name, env.now))
 
-    # Perform the job
-    print('Customer %d being served at %d' % (cust_num, env.now))
-    yield env.timeout(job_duration)
-    print('Customer %d finished being served at %d' % (cust_num, env.now))
-
-
-
-
-
-'''
-class Car(object):
-  def __init__(self, env):
-    self.env = env
-    # Start the run process everytime an instance is created.
-    self.action = env.process(self.run())
-
-  def run(self):
-    while True:
-      print('Start parking and charging at %d' % self.env.now)
-      charge_duration = 5
-
-      # We yield the process that process() returns
-      # to wait for it to finish
-
-      #We may get interrupted while charging the battery
-      try:
-        yield self.env.process(self.charge(charge_duration))
-      except simpy.Interrupt:
-        # When we received an interrupt, we stop chargin and
-        # switch to the "driving" state
-        print('Was interrupted. Hope the battery is full enough!')
-
-      # The charge process has finished and
-      # we can start driving again
-      print('Start driving at %d' % self.env.now)
-      trip_duration = 2
-      yield self.env.timeout(trip_duration)
-
-  def charge(self, duration):
-    yield self.env.timeout(duration)
-'''
-
-
-'''
-def Car(env):
-  while True:
-    print('Start parking at %d' % env.now)
-    parking_duration = 5
-    yield env.timeout(parking_duration)
-
-    print('Start driving at %d' % env.now)
-    trip_duration = 2
-    yield env.timeout(trip_duration)
-'''
+            yield env.timeout(job_duration)
+            print('FIN\t%s finished being served at %d' % (name, env.now))
--- 4470-mod-sim/sim-mech-shop/sim.py
+++ 4470-mod-sim/sim-mech-shop/sim.py
@@ -1,21 +1,73 @@
 # python3.6
 
 import simpy
+import random
+import numpy as np
 from customer import customer
-# from mech_shop_time import interarrivaltime, season
+
+NUM_CUSTOMERS = 100000000000000
+NUM_DAYS = 10
+IAT = 10
+DAY_LENGTH = 420
+day = 0
+ARR_per_day = []
+BYE_per_day = []
+
+def source(env, NUM_CUSTOMERS, interval, mech, DAY_LENGTH, ARR_per_day, BYE_per_day, day):
+    count_ARR = 0
+    count_BYE = 0
+    """Source generates customers randomly"""
+    for i in range(NUM_CUSTOMERS):
+        count_ARR += 1
+
+        job_duration = joblength()
+        job_duration = np.random.uniform(job_duration - 5, job_duration + 5,)
+
+        time_of_day = env.now % DAY_LENGTH
+
+        if (time_of_day + job_duration > DAY_LENGTH):
+            count_BYE += 1
+            c = customer(env, 'Customer %05d' % i, mech, DAY_LENGTH, job_duration, status=1)
+
+        else:
+            c = customer(env, 'Customer %05d' % i, mech, DAY_LENGTH, job_duration, status=0)
+
+        env.process(c)
+
+        # wait until next cust arrives
+        t = random.expovariate(1.0 / interval)
+
+        # track the number of customers that head to the shop per day
+        if ((env.now % DAY_LENGTH) + t > DAY_LENGTH):
+            ARR_per_day.append(count_ARR)
+            BYE_per_day.append(count_BYE)
+            # reset daily variables, and increment day counter
+            day += 1
+            count_ARR = 0
+            count_BYE = 0
+        
+        yield env.timeout(t)
 
 
-# can also change it to run strictly on time.
-NUM_CUSTOMERS = 10000
+def joblength():
+    n = random.randint(0,3)
+    if (n == 0):
+        return 15
+    elif (n == 1):
+        return 20
+    elif (n == 2):
+        return 25
+    else:
+        return 30
 
 env = simpy.Environment()
-# Create three mechanics
 mech = simpy.Resource(env, capacity=3)
 
-for cust_num in range(NUM_CUSTOMERS):
-  env.process(customer(env, mech, cust_num, 5, 7))
-  # env.process(customer(env, mech, customer, interarrivaltime(env, season(env)), 7))
+env.process(source(env, NUM_CUSTOMERS, IAT, mech, DAY_LENGTH, ARR_per_day, BYE_per_day, day))
+env.run(until=DAY_LENGTH*NUM_DAYS)
 
-env.run()
-# each day is 420 minutes
-# env.run(until=15*420)
+print('\n\nARR_PER_DAY')
+print(ARR_per_day)
+
+print('\n\nBYE_PER_DAY')
+print(BYE_per_day)
